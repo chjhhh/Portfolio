6b3bdfb733f23ec6aa959e06a501729463f71bb7
chjhhh <cheonj288@gmail.com>
2022-04-29 17:12:01 +0900

    깃 버튼 수정

--- js/index2.js
+++ js/index2.js
@@ -4,13 +4,22 @@ $(document).ready(function(){
         if($(this).scrollTop() > 600){
             $('header').fadeIn();
             $('.top').fadeIn();
-            $('.pc-shortcut').fadeIn(); // pc에서만 보이게
         } else{
             $('header').fadeOut();
             $('.top').fadeOut();
+        }
+    });
+
+    // 스크롤 내렸을 때 header와 top버튼 보이기
+    $(window).scroll(function(){
+        let windowWidth = $(window).width();
+        if($(this).scrollTop() > 600 && windowWidth > 1200){
+            $('.pc-shortcut').fadeIn(); // pc에서만 보이게
+        } else{
             $('.pc-shortcut').fadeOut(); // 수정해야 됨
         }
     });
+
 
     // 스크롤바 움직일 때 각 티켓 section에 그림자 추가, 삭제
     
